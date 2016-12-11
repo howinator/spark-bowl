fc5986d1db613fef54a234cc5bbc17f2e039afd5
howinator <howie.benefiel@unizin.org>
2016-12-10 19:49:47 -0600

    Start making backend

--- lambda/index.py
+++ lambda/index.py
@@ -1,1 +1,13 @@
-import urllib2
+import json, requests, logging
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+
+def lambda_handler(event, context):
+    """ Handles the routing of events to specific
+
+    try:
+        detail = event['detail']
+        request_param = detail['requestParameters']
+        event_name = detail['eventName']
