947454f48972ec50476af4a1326b18c905d5876d
howinator <howie.benefiel@unizin.org>
2016-12-30 10:49:51 -0600

    Working build

--- lambda/src/auxiliary.py
+++ lambda/src/auxiliary.py
@@ -6,13 +6,13 @@ class WrongApplicationIdException(Exception):
     pass
 
 
-if 'SPARKABOWL_DEBUG_MODE' not in os.environ:
-    DEVELOPMENT_ENV = True
-
-
 def set_logger():
+    if 'SPARKABOWL_DEBUG_MODE' not in os.environ:
+        dev_env = True
+    else:
+        dev_env = False
     log = logging.getLogger()
-    if DEVELOPMENT_ENV:
+    if dev_env:
         myhandler = logging.StreamHandler()  # writes to stderr
         myformatter = logging.Formatter(fmt='%(levelname)s: %(message)s')
         myhandler.setFormatter(myformatter)
