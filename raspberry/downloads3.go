d7fd0bf02489e97e2009b006f28de47ae599c2a3
howinator <howie.benefiel@unizin.org>
2017-01-13 00:06:50 -0600

    Working golang code to download file from s3

--- raspberry/downloads3.go
+++ raspberry/downloads3.go
@@ -2,12 +2,14 @@ package main
 
 import (
     "fmt"
+    "os"
+    "io"
 
     "github.com/aws/aws-sdk-go/aws"
     "github.com/aws/aws-sdk-go/aws/session"
     "github.com/aws/aws-sdk-go/service/sts"
     "github.com/aws/aws-sdk-go/service/s3"
-    "github.com/aws/aws-sdk-go/service/s3/s3manager"
+    // "github.com/aws/aws-sdk-go/service/s3/s3manager"
 )
 
 func main() {
@@ -16,12 +18,17 @@ func main() {
         panic(err)
     }
 
-    stssvc := sts.New(sess)
-    params := &sts.AssumeRoleInput{
+    stssvc := sts.New(sess, &aws.Config{Region: aws.String("us-east-1")})
+    stsparams := &sts.AssumeRoleInput{
         RoleArn: aws.String("arn:aws:iam::742524706181:role/ConfigKeyAccess"),
         RoleSessionName: aws.String("golangAssumeRoleTest"),
     }
-    resp, err := stssvc.AssumeRole(params)
+    stsresp, err := stssvc.AssumeRole(stsparams)
+    if err != nil {
+        fmt.Println(err.Error())
+        return
+    }
+    fmt.Println(stsresp)
     svc := s3.New(sess, &aws.Config{Region: aws.String("us-east-1")})
     params := &s3.GetObjectInput{
         Bucket: aws.String("howinator-config"),
@@ -33,6 +40,11 @@ func main() {
         fmt.Println(err.Error())
         return
     }
+
+    outFile, err := os.Create("/tmp/config.yml")
+    defer outFile.Close()
+
+    _, err = io.Copy(outFile, resp.Body)
 
     fmt.Println(resp)
 }
